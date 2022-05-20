4c95f8e30a41e1fa3e5f7324c816b20aa4d5346a
Pedro Girardi <prg@fastmail.com>
2022-05-20 17:36:23 -0300

    Fix wordPattern

--- src/nightincode/extension.cljs
+++ src/nightincode/extension.cljs
@@ -1,1 +1,14 @@
-(ns nightincode.extension)
+(ns nightincode.extension
+  (:require
+   ["vscode" :as vscode]))
+
+(def word-pattern
+  "Clojure symbol regex."
+  #"(?:/|[^\s,;\(\)\[\]{}\"`~@\^\\][^\s,;\(\)\[\]{}\"`~@\^\\]*)")
+
+(defn activate [^js context]
+  (let [^js languages (.-languages vscode)]
+    (.setLanguageConfiguration languages "clojure" #js {:wordPattern word-pattern})))
+
+(defn deactivate []
+  nil)
