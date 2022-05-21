b0b1f446fe6cdaf624996e7c3cd4eb08988ec6a2
Pedro Girardi <prg@fastmail.com>
2022-05-21 15:27:53 -0300

    Server WIP

--- src/nightincode/server.clj
+++ src/nightincode/server.clj
@@ -1,1 +1,77 @@
-(ns nightincode.server)
+(ns nightincode.server
+  (:import
+   (java.util.concurrent
+    CompletableFuture)
+
+   (org.eclipse.lsp4j
+    InitializeResult
+    ServerCapabilities
+    TextDocumentSyncKind
+    CompletionOptions)
+
+   (org.eclipse.lsp4j.services
+    LanguageClient
+    LanguageServer
+    LanguageClientAware)))
+
+;; The compiler will emit warnings when reflection is needed
+;; to resolve Java method calls or field accesses.
+(set! *warn-on-reflection* true)
+
+(def state-ref (atom nil))
+
+(defn complete ^CompletableFuture [value]
+  (doto (CompletableFuture.) (.complete value)))
+
+(def server
+  (reify LanguageServer LanguageClientAware
+
+    ;; The initialize request is sent as the first request from the client to the server.
+    ;; If the server receives requests or notifications before the initialize request, it should act as follows:
+    ;;  * for a request, the response should be errored with: ResponseErrorCode.ServerNotInitialized. The message can be picked by the server.
+    ;;  * notifications should be dropped, except for the exit notification. This will allow the client to exit a server without an initialize request.
+    ;;
+    ;; Until the server has responded to the initialize request with an InitializeResult,
+    ;; the client must not send any additional requests or notifications to the server.
+    ;;
+    ;; During the initialize request, the server is allowed to send the notifications
+    ;; window/showMessage, window/logMessage, and telemetry/event,
+    ;; as well as the request window/showMessageRequest, to the client.
+    (initialize [_ _params]
+      (let [initializer (InitializeResult. (ServerCapabilities.))
+
+            ^ServerCapabilities capabilities (.getCapabilities initializer)]
+
+        ;; Documents are synced by always sending the full content of the document.
+        (.setTextDocumentSync capabilities TextDocumentSyncKind/Full)
+
+        ;; The server provides completion support.
+        (.setCompletionProvider capabilities (CompletionOptions.))
+
+        (reset! state-ref {:TextDocumentService nil
+                           :WorkspaceService nil})
+
+        (complete initializer)))
+
+    (getTextDocumentService [_]
+      (:TextDocumentService @state-ref))
+
+    (getWorkspaceService [_]
+      (:WorkspaceService @state-ref))
+
+    (^void connect [_ ^LanguageClient client]
+     (swap! state-ref assoc :LanguageClient client)
+
+     nil)
+
+    ;; The shutdown request is sent from the client to the server. It asks the server to shutdown,
+    ;; but to not exit (otherwise the response might not be delivered correctly to the client).
+    ;; There is a separate exit notification that asks the server to exit.
+    (shutdown [_]
+      (swap! state-ref assoc :exit-status-code 0)
+
+      nil)
+
+    ;; A notification to ask the server to exit its process.
+    (exit [_]
+      (System/exit (or (:exit-status-code @state-ref) -1)))))
