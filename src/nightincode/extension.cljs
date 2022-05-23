d15f7ebf678aa7151605d9fe1d1db15ca78a4278
Pedro Girardi <prg@fastmail.com>
2022-05-23 11:31:16 -0300

    Logs

--- src/nightincode/extension.cljs
+++ src/nightincode/extension.cljs
@@ -1,14 +1,29 @@
 (ns nightincode.extension
   (:require
-   ["vscode" :as vscode]))
+   ["vscode" :as vscode]
+   ["path" :as path]
+   ["vscode-languageclient/node" :as client]))
 
 (def word-pattern
   "Clojure symbol regex."
   #"(?:/|[^\s,;\(\)\[\]{}\"`~@\^\\][^\s,;\(\)\[\]{}\"`~@\^\\]*)")
 
 (defn activate [^js context]
-  (let [^js languages (.-languages vscode)]
-    (.setLanguageConfiguration languages "clojure" #js {:wordPattern word-pattern})))
+  (let [^js output (vscode/window.createOutputChannel "Nightincode")
+
+        jar-path (path/join (.-extensionPath context) "server.jar")
+
+        server-options #js{:run #js{:command "java" :args #js["-jar", jar-path]}
+                           :debug #js{:command "java" :args #js["-jar", jar-path]}}
+
+        client-options #js{:documentSelector #js[#js{:language "clojure"}]
+                           :outputChannel output}]
+
+    (vscode/languages.setLanguageConfiguration "clojure" #js {:wordPattern word-pattern})
+
+    (.appendLine output (str "Extension Path: " (.-extensionPath context) "\n"))
+
+    (.appendLine output "Happy coding!")))
 
 (defn deactivate []
   nil)
