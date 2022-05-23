5fa3500a01423c4fd629cf01423d7bd6f34ee69f
Pedro Girardi <prg@fastmail.com>
2022-05-23 16:49:45 -0300

    Don't start server

--- src/nightincode/extension.cljs
+++ src/nightincode/extension.cljs
@@ -8,26 +8,33 @@
   "Clojure symbol regex."
   #"(?:/|[^\s,;\(\)\[\]{}\"`~@\^\\][^\s,;\(\)\[\]{}\"`~@\^\\]*)")
 
+#_(defn activate [^js context]
+    (let [^js output (vscode/window.createOutputChannel "Nightincode")
+
+          server-JAR-path (path/join (.-extensionPath context) "server.jar")
+
+          server-options #js{:run #js{:command "java" :args #js["-jar", server-JAR-path]}
+                             :debug #js{:command "java" :args #js["-jar", server-JAR-path]}}
+
+          client-options #js{:documentSelector #js[#js{:language "clojure"}]
+                             :outputChannel output}
+
+          client (client/LanguageClient. "Nightincode" "Nightincode" server-options client-options)
+
+          ^js subscriptions (.-subscriptions context)]
+
+      (vscode/languages.setLanguageConfiguration "clojure" #js {:wordPattern word-pattern})
+
+      (.appendLine output (str "Extension Path: " (.-extensionPath context) "\n"))
+
+      (.push subscriptions (.start client))))
+
 (defn activate [^js context]
-  (let [^js output (vscode/window.createOutputChannel "Nightincode")
-
-        server-JAR-path (path/join (.-extensionPath context) "server.jar")
-
-        server-options #js{:run #js{:command "java" :args #js["-jar", server-JAR-path]}
-                           :debug #js{:command "java" :args #js["-jar", server-JAR-path]}}
-
-        client-options #js{:documentSelector #js[#js{:language "clojure"}]
-                           :outputChannel output}
-
-        client (client/LanguageClient. "Nightincode" "Nightincode" server-options client-options)
-
-        ^js subscriptions (.-subscriptions context)]
+  (let [^js output (vscode/window.createOutputChannel "Nightincode")]
 
     (vscode/languages.setLanguageConfiguration "clojure" #js {:wordPattern word-pattern})
 
-    (.appendLine output (str "Extension Path: " (.-extensionPath context) "\n"))
-
-    (.push subscriptions (.start client))))
+    (.appendLine output (str "Extension Path: " (.-extensionPath context) "\n"))))
 
 (defn deactivate []
   nil)
