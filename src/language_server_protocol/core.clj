3123d244242776f59f3ade5ad22176b0f3528721
Pedro Girardi <prg@fastmail.com>
2022-05-26 17:54:04 -0300

    Test server

--- src/language_server_protocol/core.clj
+++ src/language_server_protocol/core.clj
@@ -2,6 +2,18 @@
   (:require
    [clojure.string :as str]
    [clojure.data.json :as json])
+
+  (:import
+   (java.io
+    PipedInputStream
+    PipedOutputStream)
+
+   (java.util.concurrent
+    CompletableFuture)
+
+   (org.eclipse.lsp4j.jsonrpc
+    Endpoint))
+
   (:gen-class))
 
 (defn -main [& _]
@@ -23,3 +35,33 @@
 
       (pr result))))
 
+
+(comment
+
+  ;; An endpoint is a generic interface that accepts jsonrpc requests and notifications.
+  (def endpoint
+    (reify Endpoint
+      (^void notify [_ method parameter]
+       (tap> ['notify method parameter]))
+
+      (^CompletableFuture request [_ method parameter]
+       (tap> ['request method parameter])
+
+       (CompletableFuture/completedFuture
+         (let [[_ jsonrpc] (str/split (slurp *in*) #"\r\n\r\n")
+
+               {:strs [id]} (json/read-str jsonrpc)
+
+               result (json/write-str {:id id
+                                       :jsonrpc "2.0"
+                                       :result
+                                       {:capabilities
+                                        {:hoverProvider true}
+
+                                        :serverInfo
+                                        {:name "Nightincode"}}})]
+
+           (format "Content-Length: %s\r\n\r\n%s" (alength (.getBytes result)) result))))))
+
+
+  )
