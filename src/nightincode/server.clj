eb6811b496f46c6aefadf4ef89249a7a1fb41dac
Pedro Girardi <prg@fastmail.com>
2024-01-02 11:17:59 -0300

    More docs

--- src/nightincode/server.clj
+++ src/nightincode/server.clj
@@ -537,6 +537,15 @@
 
   ;; The base protocol offers support for request cancellation.
   ;;
+  ;; A request that got canceled still needs to return from the server and send a response back.
+  ;; It can not be left open / hanging. This is in line with the JSON-RPC protocol that requires
+  ;; that every request sends a response back.
+  ;;
+  ;; In addition it allows for returning partial results on cancel.
+  ;;
+  ;; If the request returns an error response on cancellation it is advised
+  ;; to set the error code to ErrorCodes.RequestCancelled.
+  ;;
   ;; https://microsoft.github.io/language-server-protocol/specifications/lsp/3.17/specification/#cancelRequest
 
   nil)
